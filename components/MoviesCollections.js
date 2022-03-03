bd68d174fb68292378de89123e90407a089f5dc1
Govind Kumar <you@example.com>
2022-03-03 18:34:14 +0530

    Something added

--- components/MoviesCollections.js
+++ components/MoviesCollections.js
@@ -1,7 +1,7 @@
 import MovieThumbnail from "./MovieThumbnail"
 const MoviesCollections = ({results,title,isMovie}) => {
   return (
-    <div className="relative flex flex-col  space-y-2 my-10 px-0 md:px-10  mx-auto overflow-y-hidden">
+    <div className="relative flex flex-col  space-y-2 my-10 mx-auto overflow-y-hidden">
          <h2 className="font-semibold  ">{title}</h2>
         <div className="flex scrollbar-hide p-2 space-x-2 md:space-x-5 overflow-y-hidden overflow-x-scroll w-screen max-w-full">
         {
